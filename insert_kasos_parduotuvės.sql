31ad5658e45277ddee1af69f6e3dc3bee3735079
Jurij Ambros <jurij.ambros@gmail.com>
2021-02-27 10:56:37 +0200

    parduotuvės kasos insert

---
+++ insert_kasos_parduotuvės.sql
@@ -0,0 +1,12 @@
+USE `3PL1_parduotuvė`;
+
+#INSERT INTO parduotuvės (Adresas) VALUES ('Vivulskio g. 30, Vilnius');
+
+
+/*INSERT INTO Kasos (ParduotuvėsId, Numeris) VALUES
+(1, 'P3 CITO'),
+(1, 'P2 CITO'),
+(1, 'P1 CITO');*/
+
+
+
